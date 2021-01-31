09e974bcb06c1d005c560c49233eede13b3bcfbc
jaideep1163 <66325920+jaideep1163@users.noreply.github.com>
2021-01-31 11:34:55 +0530

    Added Add Email Function

---
+++ createDB.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+# Conn
+conn = sqlite3.connect("PasswordStorage.db")
+
+# Create the Cursor
+c = conn.cursor()
+
+c.execute("""CREATE TABLE passwords (
+username text,
+website text,
+password text
+)""")
+
+# Commit
+conn.commit()
+
+# Close the Conn
+
+Conn.close()
